5acd4eef851a338d9f12d2d7f4c883cca13e662b
Hitesh A H <hitesh.harikumar@teranet.ca>
2023-11-15 12:31:09 +0530

    Custom Decorator- Unsubscribe

--- src/app/Full-Stack-Site/listing-page/listing-page.component.ts
+++ src/app/Full-Stack-Site/listing-page/listing-page.component.ts
@@ -2,12 +2,14 @@ import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { FakeProduct } from '../fake-apimodels';
 import { FakeAPIStoreService } from '../fake-apistore.service';
+import { Unsubscribe } from 'src/app/Custom-Decorators/unsubscribe';
 
 @Component({
   selector: 'app-listing-page',
   templateUrl: './listing-page.component.html',
   styleUrls: ['./listing-page.component.scss']
 })
+//@Unsubscribe()
 export class ListingPageComponent implements OnInit{
   public productList$!:Observable<FakeProduct[]>;
   constructor(private fakeAPIService: FakeAPIStoreService){}
