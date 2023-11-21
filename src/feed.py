2a92724920520433179aeb8275c8d4ad74a030de
Hitesh A H <118159423+hiteshah185@users.noreply.github.com>
2023-11-21 23:45:34 +0530

    Update feed.py

--- src/feed.py
+++ src/feed.py
@@ -1,6 +1,6 @@
 import yaml;
 import xml.etree.ElementTree as xml_tree;
-with open('feed.yaml','r') as file:
+with open('src/feed.yaml','r') as file:
     yaml_data = yaml.safe_load(file)
 
     rss_element = xml_tree.Element('rss' ,{
