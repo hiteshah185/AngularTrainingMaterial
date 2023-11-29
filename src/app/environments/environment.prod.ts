a480b8e061834e71c8ae63af462d5166405f69b7
Hitesh Harikumar <hitesh.harikumar@teranet.ca>
2023-11-29 15:02:44 +0530

    Environment - For Build

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    appName: 'Angular Learning Material',
+    envName: 'PROD',
+    production: true,
+    test: false,
+    mainDomain: "http://localhost:8080/api/prod"
+}
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    appName: 'Angular Learning Material',
+    envName: 'DEV',
+    production: false,
+    test: false,
+    mainDomain: "http://localhost:8080/api/dev"
+}
