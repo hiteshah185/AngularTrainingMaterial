46d9e7f4b2c9b3a5020d55ee0f3a9d375c5d9874
Hitesh Harikumar <hitesh.harikumar@teranet.ca>
2023-12-13 18:25:11 +0530

    Loading Spinner - Improvised

--- src/app/services/loader.service.ts
+++ src/app/services/loader.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoaderService {
   private isLoading: boolean = false;
-  constructor() { }
+  public spinner$: Subject<any>;
+  constructor() {
+    this.spinner$ = new Subject<any>();
+  }
   setLoading(loading: boolean): void {
     this.isLoading = loading;
   }
   getLoading(): boolean {
     return this.isLoading;
   }
+  showSpinner() {
+    this.spinner$.next(true);
+  }
+  hideSpinner() {
+    this.spinner$.next(false);
+  }
+
 }
--- src/app/spinner/spinner.component.ts
+++ src/app/spinner/spinner.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { LoaderService } from '../services/loader.service';
+import { LoggingService } from '../services/logging.service';
 
 @Component({
   selector: 'app-spinner',
@@ -7,7 +8,20 @@ import { LoaderService } from '../services/loader.service';
   styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent {
+  //Used as a Global Loading Spinner
+  public showSpinner: boolean = false;
   public _loader = inject(LoaderService)
-  constructor() { }
+  constructor(
+    private _logService: LoggingService
+  ) {
+    this._loader.spinner$.subscribe((data: boolean) => {
+      setTimeout(() => {
+        if (data) {
+          this.showSpinner = data;
+        }
+      });
+      this._logService.logging(`Show Spinner:${this.showSpinner}`);
+    })
+  }
 
 }
