deeddbbb2897b332581b44be178fcac45077b90a
Hitesh A H <hitesh.harikumar@teranet.ca>
2024-01-18 21:48:59 +0530

    RxJs: catchError() method

--- src/app/Example-ngRx/movie-data.service.ts
+++ src/app/Example-ngRx/movie-data.service.ts
@@ -12,20 +12,18 @@ export class MovieDataService {
   constructor(private _http: HttpClient) { }
   getMovies(): Observable<Movie[]> {
     return this._http.get<Movie[]>(this.baseURL).pipe(
-      catchError((error: HttpErrorResponse) => {
-        console.error(error);
-        return throwError(error);
-      })
+      catchError(this.handleError)
     );
   }
 
   addMovies(movie: Movie): Observable<Movie> {
     movie.id = 0;
     return this._http.post<Movie>(this.baseURL, movie).pipe(
-      catchError((err: HttpErrorResponse) => {
-        console.error(err);
-        return throwError(err);
-      })
+      catchError(this.handleError)
     );
+  }
+  private handleError(err: HttpErrorResponse) {
+    console.error(err);
+    return throwError(err);
   }
 }
