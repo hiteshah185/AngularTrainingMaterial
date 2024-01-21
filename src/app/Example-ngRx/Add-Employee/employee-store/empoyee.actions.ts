6f6584b833c7bca69ff380ecc2b738124b1673a8
Hitesh A H <hitesh.harikumar@teranet.ca>
2024-01-21 10:04:32 +0530

    NgRx - export strategy & createActionGroup

--- src/app/Example-ngRx/Add-Employee/employee-store/empoyee.actions.ts
+++ src/app/Example-ngRx/Add-Employee/employee-store/empoyee.actions.ts
@@ -1,7 +1,22 @@
-import { createAction } from "@ngrx/store";
+import { createAction, createActionGroup, props } from "@ngrx/store";
 import { Employee } from "src/app/entity/employee";
 
 export const addEmployee = createAction(
     '[Employee] Add Employee',
     (newEmployee: Employee) => ({ newEmployee })
 );
+
+
+//Add more actions into the below const group to avoid conflicting imports.
+export const EmployeeStandardActions = {
+    addEmployee
+};
+
+export const EmployeeStandardActions_usingActionGroup =
+    createActionGroup({
+        source: 'Employee',
+        events: {
+            addAnEmployee: props<{ employee: Employee }>()
+        },
+    });
+const { addAnEmployee } = EmployeeStandardActions_usingActionGroup;
