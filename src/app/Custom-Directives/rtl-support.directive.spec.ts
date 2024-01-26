ba0fa7a9b0e83b203c1eb3e1e9b75c51fc83b4ca
Hitesh A H <hitesh.harikumar@teranet.ca>
2024-01-26 19:10:13 +0530

    Deleted an rtl spec

--- src/app/Custom-Directives/rtl-support.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RtlSupportDirective } from './rtl-support.directive';
-
-describe('RtlSupportDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RtlSupportDirective();
-    expect(directive).toBeTruthy();
-  });
-});
