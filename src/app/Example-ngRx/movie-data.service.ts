aca16de0a5411fc347dbf56ff45c93341507848f
Hitesh Harikumar <hitesh.harikumar@teranet.ca>
2024-01-29 12:25:03 +0530

    RxJs - merge & scan Operator - Add Fake Movie

--- src/app/Example-ngRx/movie-data.service.ts
+++ src/app/Example-ngRx/movie-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, catchError, tap, throwError, map, BehaviorSubject, combineLatest } from 'rxjs';
+import { Observable, catchError, tap, throwError, map, BehaviorSubject, combineLatest, Subject, merge, scan } from 'rxjs';
 import { ICelebrity, IMovie, Movie } from './Movie.model';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
@@ -32,6 +32,8 @@ export class MovieDataService {
     .pipe(
       // tap(c => console.log(`Data from celebrity API:`, JSON.stringify(c))),
       catchError(this.handleError));
+
+
   private selectedCelebrity = new BehaviorSubject<number>(0);
   public selectedCelebrity$ = combineLatest([this.celebrity$, this.selectedCelebrity.asObservable()])
     .pipe(
@@ -39,7 +41,28 @@ export class MovieDataService {
         celebrities.find(celebrity => celebrity.id === selectedCelebrity)),
       tap(celebrity => { console.log('Selected Celebrity:', celebrity); })
     );
+
+  private insertedMovie = new Subject<IMovie>();
+  //public insertedMovie$ = this.insertedMovie.asObservable();
+
+  public moviesWithInserted$ = merge(
+    this.movie$, this.insertedMovie.asObservable()
+  ).pipe(
+    scan((acc, value) => (value instanceof Array) ? [...value] : [...acc, value], [] as IMovie[])
+  );
+
+  private fakeMovie(): IMovie {
+    return {
+      id: 22,
+      name: 'Teranet Movie',
+      earning: 100000,
+      userName: 'TERANET-TRV-RPS',
+      celebrity: 100
+    } as IMovie;
+  }
   constructor(private _http: HttpClient) { }
+
+
   getMovies(): Observable<Movie[]> {
     return this._http.get<Movie[]>(this.baseURL).pipe(tap(data => console.log('All Movie Data:', JSON.stringify(data))),
       catchError(this.handleError)
@@ -55,6 +78,10 @@ export class MovieDataService {
 
   selectedCelebrityChanged(selectedCelebrityId: number): void {
     this.selectedCelebrity.next(selectedCelebrityId);
+  }
+  addAFakeMovie(newMovie?: IMovie) {
+    newMovie = newMovie || this.fakeMovie();
+    this.insertedMovie.next(newMovie);
   }
   private handleError(err: HttpErrorResponse): Observable<never> {
     let errorMessage: string;
