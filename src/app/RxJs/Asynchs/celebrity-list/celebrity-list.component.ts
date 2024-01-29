a40f27b1b41209ac43d59e5469d7c93f6844fd01
Hitesh Harikumar <hitesh.harikumar@teranet.ca>
2024-01-29 10:11:18 +0530

    RxJs - Celebrity List Component

---
+++ src/app/RxJs/Asynchs/celebrity-list/celebrity-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EMPTY, Observable, Subject, catchError } from 'rxjs';
+import { MovieDataService } from 'src/app/Example-ngRx/movie-data.service';
+import { ICelebrity } from 'src/app/Example-ngRx/Movie.model';
+
+@Component({
+  selector: 'app-celebrity-list',
+  templateUrl: './celebrity-list.component.html',
+  styleUrls: ['./celebrity-list.component.scss']
+})
+export class CelebrityListComponent {
+  pageTitle: string = 'Celebrity List';
+  private errorMessageSubject = new Subject<string>();
+  errorMessage$ = this.errorMessageSubject.asObservable();
+  celebrity$: Observable<ICelebrity[]> = this._movieService.celebrity$.pipe(
+    catchError(err => {
+      this.errorMessageSubject.next(err);
+      return EMPTY;
+    })
+  );
+  selectedCelebrity$ = this._movieService.selectedCelebrity$;
+  constructor(private _movieService: MovieDataService) { }
+
+  onSelectedCelebrity(celebrityId: number) {
+    this._movieService.selectedCelebrityChanged(celebrityId);
+  }
+}
