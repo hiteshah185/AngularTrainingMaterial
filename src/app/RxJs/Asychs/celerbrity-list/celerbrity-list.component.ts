d2eaa8e9742fd510b5ead6955be8ab87c44e1455
Hitesh A H <hitesh.harikumar@teranet.ca>
2024-02-02 14:49:13 +0530

    Removed celebrity components

---
+++ src/app/RxJs/Asychs/celerbrity-list/celerbrity-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-celerbrity-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './celerbrity-list.component.html',
+  styleUrls: ['./celerbrity-list.component.scss']
+})
+export class CelerbrityListComponent {
+
+}
