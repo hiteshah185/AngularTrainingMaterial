2124011d74e7b2e2bf8c0d8bd7d697b347f78278
Hitesh A H <hitesh.harikumar@teranet.ca>
2024-02-04 21:41:29 +0530

    Edit Course Component

---
+++ src/app/Example-ngRx/Courses/edit-course/edit-course.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-edit-course',
+  templateUrl: './edit-course.component.html',
+  styleUrls: ['./edit-course.component.scss']
+})
+export class EditCourseComponent {
+
+}
